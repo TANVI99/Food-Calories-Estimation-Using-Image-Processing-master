661de4d38460d6dfae592c5ccfe8f271735b474e
TANVI99 <32447610+TANVI99@users.noreply.github.com>
2021-04-13 19:57:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
+
 from flask import Flask, render_template,flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from flask_ngrok import run_with_ngrok
 UPLOAD_FOLDER = '/uploads'
 app = Flask(__name__, )
 run_with_ngrok(app)
+
 @app.route("/")
 def project(name=None):
     return render_template('project.html',name=name)
 
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
-		
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+		
 app.run()
