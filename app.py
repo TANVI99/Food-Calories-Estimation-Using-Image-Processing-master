b3393f0572b47f705050f84b75540494b8449b51
TANVI99 <32447610+TANVI99@users.noreply.github.com>
2021-04-13 15:34:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-
 from flask import Flask, render_template
 from flask_ngrok import run_with_ngrok
 app = Flask(__name__, )
 run_with_ngrok(app)
 @app.route("/")
-def project():
-    return render_template('project.html')
+def project(name=None):
+    return render_template('project.html',name=name)
 app.run()
