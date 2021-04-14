d5b812e4644e903ace81b64b648703ccf07b1c99
TANVI99 <32447610+TANVI99@users.noreply.github.com>
2021-04-14 15:14:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-
 from flask import Flask, render_template,flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from flask_ngrok import run_with_ngrok
-app.config['UPLOAD_FOLDER'] = '/uploads'
 app = Flask(__name__, )
+app.config['UPLOAD_PATH'] = '/uploads'
 run_with_ngrok(app)
 
 @app.route("/")
@@ -15,6 +14,7 @@ def upload_file():
    if request.method == 'POST':
       f = request.files['file']
       f.save(secure_filename(f.filename))
+      uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
       return 'file uploaded successfully'
 		
 app.run()
